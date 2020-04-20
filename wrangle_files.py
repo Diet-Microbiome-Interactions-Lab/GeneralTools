602e9046e4261e07eed5664e48e3751df2e34de8
ddeemerpurdue <64015522+ddeemerpurdue@users.noreply.github.com>
2020-04-20 17:33:38 -0400

    Create wrangle_files.py

---
+++ wrangle_files.py
@@ -0,0 +1,27 @@
+"""
+Program to grab a bunch of .* files and
+put them into the same directory. If 'make'
+is specified, you make the directory specified
+as <directory>
+"""
+import os
+import shutil
+import sys
+
+
+def move_files(string, directory, make = False):
+	desfiles = []
+	if make == True:
+		os.mkdir(directory)
+	else:
+		pass
+	for root, dirs, files in os.walk("."):
+		for file in files:
+			if string in file:
+				desfiles.append(os.path.join(root, file))
+	for f in desfiles:
+		shutil.copy(f, directory)
+
+
+if __name__ == "__main__":
+	move_files(sys.argv[1], sys.argv[2], sys.argv[3])
