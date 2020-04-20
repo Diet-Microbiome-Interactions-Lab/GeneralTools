a23afe9462dc4d517bb21ada4461af5b08cf3234
ddeemerpurdue <64015522+ddeemerpurdue@users.noreply.github.com>
2020-04-20 11:07:19 -0400

    Create tetranucleotide_freq.py

---
+++ tetranucleotide_freq.py
@@ -0,0 +1,32 @@
+"""
+Obtain tetranucleotide frequency for a
+given string within a nucleotide sequnce
+"""
+
+import sys
+import csv
+
+mysequence = sys.argv[1:]
+
+def tetranucleotide_freq(sequence):
+	nuc = ["A", "T", "G", "C"]
+	mer_list = []
+	for i in range(4):
+		for j in range(4):
+			for k in range(4):
+				for l in range(4):
+					mer = nuc[i] + nuc[j] + nuc[k] + nuc[l]
+					mer_list.append(mer)
+	sequence = sequence.upper()
+	length = len(sequence)
+	freq_hash = {}
+	for i in range(0, length - 3):
+		mer = sequence[i:i+4]
+		if mer not in freq_hash:
+			freq_hash[mer] = 0
+		freq_hash[mer] = freq_hash[mer] + 1
+	return freq_hash
+
+
+if __name__ == "__main__":
+	tetranucleotide_freq(mysequence)
