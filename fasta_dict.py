6886e311256d660854e286bf57ce9a553331e11a
ddeemerpurdue <64015522+ddeemerpurdue@users.noreply.github.com>
2020-04-20 13:53:37 -0400

    Update fasta_dict.py

--- fasta_dict.py
+++ fasta_dict.py
@@ -1,5 +1,24 @@
+"""
+Function that requires: 1+ .FASTA files and
+writes a csv files containing: .fasta description,
+what file it came from (bin), length, and GC content
+
+Example use:
+$ python fasta_dict.py .fasta <savename.csv>
+"""
+
 import os
-import matplotlib.pyplot as plt
+import csv
+import sys
+
+# os.chdir('/Users/Dane/Documents/Lindemann/WhiteSAXApr20/\
+# MaxBin2/MB_W1_BWA')
+
+files = []
+for f in os.listdir():
+	if f.endswith(sys.argv[1]):
+		files.append(f)
+
 
 def gc_cont(string):
 	string = string.upper()
@@ -14,7 +33,8 @@ def read_fasta(file):
 		line = f.readline()
 		while line:
 			if line.startswith('>'):
-				header = line.strip()
+				header = line.strip('>')
+				header = header.strip()
 				line = f.readline()
 				nucleotides = ''
 				file_id = ''
@@ -24,5 +44,31 @@ def read_fasta(file):
 				file_id = file.strip()
 				fasta_dict[header] = [file_id,
 									  len(nucleotides),
-									  gc_cont(nucleotides)]
+									  gc_cont(nucleotides)
+									  # tetranucleotide_freq(nucleotides)]
+									  ]
 	return fasta_dict
+
+def read_multiple_fasta(files):
+	master_dict = {}
+	for file in files:
+		print(file)
+		fasta_dict = read_fasta(file)
+		print('Updating master dictionary...')
+		master_dict.update(fasta_dict)
+	print('Master dictionary updated!')
+	return master_dict
+
+
+def save_fa_dict(files, savename):
+	dictionary = read_multiple_fasta(files)
+	print('Reading in dictionary and writing to .csv...')
+	with open(savename, 'w', newline='') as csvfile:
+		writer = csv.writer(csvfile)
+		writer.writerow(dictionary) # First row (keys of dict)
+		for values in zip(*dictionary.values()):
+			writer.writerow(values)
+
+
+if __name__ == "__main__":
+	save_fa_dict(files, sys.argv[2])
