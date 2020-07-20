1f7f7232626dc2f911c6c1aab38ef5c02883e2a4
Dane <64015522+ddeemerpurdue@users.noreply.github.com>
2020-07-20 13:19:31 -0400

    Update filter_seqlength.py

--- filter_seqlength.py
+++ filter_seqlength.py
@@ -2,47 +2,36 @@
 that are at least N nucleotides long
 
 Example usage:
-$ python2 filter_seqlength.py <input.fasta> <length_threshold> <output.fasta>
+$ python filter_seqlength.py <input.fasta> <length_threshold> <output.fasta>
 '''
 
 import sys
 
 
-def read_fasta(file):
+def filter_fasta(file, size, output):
     """ Open up a .fasta file and return a dictionary containing the header
     as the key and [filename, length, & gc_cont] as values """
     fasta_dict = {}
-    with open(file) as f:
-        line = f.readline()
-        while line:
-            if line.startswith('>') or line.startswith('NODE'):
-                header = line.strip()
-                line = f.readline()
-                nucleotides = ''
-            else:
-                nucleotides = nucleotides + line.strip()
-                line = f.readline()
-                fasta_dict[header] = nucleotides
+    with open(output, 'w') as o:
+        with open(file) as f:
+            line = f.readline()
+            while line:
+                if line.startswith('>') or line.startswith('NODE'):
+                    header = line
+                    nucleotides = ""
+                    line = f.readline()
+                    while (line and not line.startswith('>')):
+                        nucleotides = nucleotides + line.strip()
+                        line = f.readline()
+                    if len(nucleotides) > int(size):
+                        o.write(header)
+                        o.write(nucleotides)
+                    else:
+                        pass
+                else:
+                    line = f.readline()
     return fasta_dict
 
 
-def filter_fasta(file, length):
-    fa = read_fasta(file)
-    for key in fa.keys():
-        if len(fa[key]) < length:
-            fa.pop(key)
-    return fa
-
-
-def write_fasta(file, length, output):
-    filtered = filter_fasta(file, int(length))
-    with open(output, 'w') as o:
-        for key, value in filtered.items():
-            o.write(key)
-            o.write('\n')
-            o.write(value)
-            o.write('\n')
-
-
 if __name__ == "__main__":
-    write_fasta(sys.argv[1], sys.argv[2], sys.argv[3])
+    filter_fasta(sys.argv[1], sys.argv[2], sys.argv[3])
