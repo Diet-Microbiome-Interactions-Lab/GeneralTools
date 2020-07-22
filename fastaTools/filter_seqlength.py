8b33e9856d3170b8fccd0939512e066bb2ced9b5
Dane <64015522+ddeemerpurdue@users.noreply.github.com>
2020-07-22 06:43:46 -0400

    Update filter_seqlength.py

--- fastaTools/filter_seqlength.py
+++ fastaTools/filter_seqlength.py
@@ -1,35 +1,29 @@
-''' Program to filter .FASTA files based on sequences
+'''
+Program to filter .FASTA files based on sequences
 that are at least N nucleotides long
 
 Example usage:
-$ python filter_seqlength.py <input.fasta> <length_threshold> <output.fasta>
+$ python filterSeqlength.py <input.fasta> <length_threshold> <output.fasta>
 '''
 
 import sys
 
 
 def filter_fasta(file, size, output):
-    """ Open up a .fasta file and return a dictionary containing the header
-    as the key and [filename, length, & gc_cont] as values """
-    fasta_dict = {}
+    """
+    Open up a .fasta file and return a dictionary containing the header
+    as the key and length as the value
+    """
     with open(output, 'w') as o:
         with open(file) as f:
-            line = f.readline()
-            while line:
-                if line.startswith('>') or line.startswith('NODE'):
-                    header = line
-                    nucleotides = ""
-                    line = f.readline()
-                    while (line and not line.startswith('>')):
-                        nucleotides = nucleotides + line.strip()
-                        line = f.readline()
-                    if len(nucleotides) > int(size):
-                        o.write(header)
-                        o.write(nucleotides)
-                    else:
-                        pass
+            for values in SimpleFastaParser():
+                defline = values[0]
+                length = len(values[1])
+                if length > size:
+                    o.write(defline)
+                    o.write(values[1])
                 else:
-                    line = f.readline()
+                    pass
     return fasta_dict
 
 
