445b3f212323240f826785bda40cb7c67b78ddb5
Dane <64015522+ddeemerpurdue@users.noreply.github.com>
2020-07-22 07:22:51 -0400

    Update and rename obtain_n_largest_seqs.py to writeLargestSequences.py

--- fastaTools/writeLargestSequences.py
+++ fastaTools/writeLargestSequences.py
@@ -1,7 +1,7 @@
 '''
 Program that takes a numeric value n and the name of a fasta
 file and returns the top n largest sequences
-$ python <myfile.fasta> <seqlength_to_filter_(int)> <output.fasta>
+$ python -i <myfile.fasta> -n <seqlength_to_filter_(int)> -o <output.fasta>
 '''
 
 import argparse
@@ -13,21 +13,25 @@ def get_fasta_dic(file):
     """ Open up a .fasta file and return a dictionary containing the header
     as the key and [filename, length, & gc_cont] as values """
     fasta_dic = {}
-    with open(output, 'w') as o:
-        with open(file) as f:
-            for values in SimpleFastaParser(f):
-                fasta_dic[values[0]] = values[1]
-        # Filter for top N entries in dictionary
+    with open(file) as f:
+        for values in SimpleFastaParser(f):
+            fasta_dic[values[0]] = values[1]
     return fasta_dic
 
 
-def return_largest_seqs(file, n, output):
-    fasta_dic = read_fasta(file)
+def write_largest_seqs(file, n, output):
+    '''
+    Function that takes in a fasta dic and writes a .fasta file
+    containing only the top n length sequences.
+    '''
+    fasta_dic = get_fasta_dic(file)
     top_n = {}
     with open(output, 'w') as o:
-        for k in sorted(fasta_dic, key=lambda k: len(fasta_dic[k]), reverse=True)[0:n]:
-            o.write(k)
-            o.write(fasta_dic[k])
+        for k in sorted(fasta_dic, key=lambda k: len(fasta_dic[k]), reverse=True)[0:int(n)]:
+            line1 = f">{k}\n"
+            o.write(line1)
+            line2 = f"{fasta_dic[k]}\n"
+            o.write(line2)
     return 0
 
 
@@ -41,4 +45,4 @@ if __name__ == "__main__":
     parser.add_argument("-o", "--Output", help="Output file name",
                     required=True)
     argument = parser.parse_args()
-    write_largest_fastas(argument.Input, argument.Length, argument.Output)
+    write_largest_seqs(argument.Input, argument.Length, argument.Output)
