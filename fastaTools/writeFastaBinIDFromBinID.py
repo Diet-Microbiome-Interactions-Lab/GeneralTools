210fe1092587a9af6f3a4d2e94ce18c171dcad3e
Dane <64015522+ddeemerpurdue@users.noreply.github.com>
2020-09-14 09:01:22 -0400

    Changes

--- fastaTools/writeFastaBinIDFromBinID.py
+++ fastaTools/writeFastaBinIDFromBinID.py
@@ -1,9 +1,8 @@
 """
-Program designed to take in a bin id file and an assembly
-file and will output:
-1.) New fasta files IF --FastaDirectory <location> is specified
+Program designed to take in an assembly file and will output:
+1.) New fasta files IF --FastaDirectory <location> is specified (and binID file)
 Note: Fasta file names will be written in the format: <binID>.fasta
-2.) New BinID file IF --Identity <filename> is specified
+2.) New BinID file IF --Identity <filename> is specified, along with 
 3.) Both IF both flags are raised
 
 
@@ -38,25 +37,19 @@ def read_binfile(binfile):
     return bins
 
 
-def write_fastas(binfile, assemblyfile, outdirectory=False, ident=False):
+def write_fastas(binfile, assemblyfile, outdirectory):
     '''
-    Function that writes a new FASTA file the defline
+    Function that writes a new FASTA file if the defline
     of the assembly matches a bin entry
     '''
-    if outdirectory is not False:
-        id_dic = {}
-        bindic = read_binfile(binfile)
-        for record in SeqIO.parse(assemblyfile, "fasta"):
-            match = record.id.split('length')[0]
-            if match in bindic:
-                outfile = f"{outdirectory}/{bindic[match]}.fasta"
-                with open(outfile, 'a') as o:
-                    o.write(f">{record.id}\n{record.seq}\n")
-                    id_dic[str(record.id)] = bindic[match]
-    if ident is not True:
-        with open(str(ident), 'w') as o:
-            for node, bins in id_dic.items():
-                o.write(f">{node}\t{bins}\n")
+    # Option A: Write new multi-fasta files to 'outdirectory'
+    bindic = read_binfile(binfile)
+    for record in SeqIO.parse(assemblyfile, "fasta"):
+        match = record.id.strip('>')
+        if match in bindic:
+            outfile = f"{outdirectory}/{bindic[match]}.fasta"
+            with open(outfile, 'a') as o:
+                o.write(f">{record.id}\n{record.seq}\n")
     return 0
 
 
@@ -69,18 +62,6 @@ if __name__ == "__main__":
                         required=True)
     parser.add_argument("-f", "--FastaDirectory",
                         help="Output directory to write to",
-                        required=False)
-    parser.add_argument("-i", "--Identity",
-                        help="Optional argument to write new binID file",
-                        required=False)
+                        required=True)
     argument = parser.parse_args()
-    if (argument.Identity and argument.FastaDirectory):
-        write_fastas(argument.Bins, argument.Assembly,
-                     ident=argument.Identity,
-                     outdirectory=argument.FastaDirectory)
-    elif argument.Identity:
-        write_fastas(argument.Bins, argument.Assembly,
-                     ident=argument.Identity)
-    else:
-        write_fastas(argument.Bins, argument.Assembly,
-                     outdirectory=argument.FastaDirectory)
+    write_fastas(argument.Bins, argument.Assembly, argument.FastaDirectory)
