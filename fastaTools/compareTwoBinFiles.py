faa9ba1172837206af25612752b78d61e334604c
Dane <64015522+ddeemerpurdue@users.noreply.github.com>
2020-12-04 09:27:36 -0500

    Update compareTwoBinFiles.py
    Increased readability

--- fastaTools/compareTwoBinFiles.py
+++ fastaTools/compareTwoBinFiles.py
@@ -1,6 +1,7 @@
 ''' 
 Author: Dane
-Program that takes two fasta files that will be compared for matching
+Date: NA
+Purpose: Program that takes two fasta files that will be compared for matching
 entries.
 This was designed to track how many entries were removed from bin sets after
 refinement, and then those removed contigs were compared to the original
@@ -10,7 +11,6 @@ Example usage:
 $ python count_same_fasta.py -1 <firstFile> -2 <secondFile> -o <outfile_name>
 '''
 import os
-import argparse
 from Bio import SeqIO
 
 
@@ -30,39 +30,43 @@ def compare_two_bins(file1, file2):
     '''
     bin1 = return_fasta_dic(file1)
     bin2 = return_fasta_dic(file2)
-    print(bin1)
-    print(bin2)
+
     shared_len = 0
-    bin1_unique_len = 0
-    bin2_unique_len = 0
-    shared = list(bin1.keys() & bin2.keys())
-    shared_quantity = len(shared)
-    for value in shared:
-        shared_len += len(bin1[value])
+    bin1Unique_length = 0
+    bin2Unique_length = 0
+
+    sharedContigs = list(bin1.keys() & bin2.keys())
+    sharedQuantity = len(sharedContigs)
+    for contig in sharedContigs:
+        shared_len += len(bin1[contig])
+
     # Difference: file1 but not file 2
-    bin1_unique = list(bin1.keys() - bin2.keys())
-    bin1_unique_quantity = len(bin1_unique)
-    for value in bin1_unique:
-        bin1_unique_len += len(bin1[value])
+    bin1Unique = list(bin1.keys() - bin2.keys())
+    bin1Unique_quantity = len(bin1Unique)
+    for value in bin1Unique:
+        bin1Unique_length += len(bin1[value])
+
     # Difference: file 2 but not file 1
-    bin2_unique = list(bin2.keys() - bin1.keys())
-    bin2_unique_quantity = len(bin2_unique)
-    for value in bin2_unique:
-        bin2_unique_len += len(bin2[value])
-    return [shared_quantity, shared_len,
-            bin1_unique_quantity, bin1_unique_len,
-            bin2_unique_quantity, bin2_unique_len]
+    bin2Unique = list(bin2.keys() - bin1.keys())
+    bin2Unique_quantity = len(bin2Unique)
+    for value in bin2Unique:
+        bin2Unique_length += len(bin2[value])
+
+    return [sharedQuantity, shared_len,
+            bin1Unique_quantity, bin1Unique_length,
+            bin2Unique_quantity, bin2Unique_length]
 
 
 def write_comparisons(file1, file2, output):
     stats = compare_two_bins(file1, file2)
-    print(stats)
+    f1 = os.path.basename(file1)
+    f2 = os.path.basename(file2)
     with open(output, 'w') as o:
         header = "\t".join(["SQuant", "SLen",
-                            f"{os.path.basename(file1)}_Quant",
-                            f"{os.path.basename(file1)}_Len",
-                            f"{os.path.basename(file2)}_Quant",
-                            f"{os.path.basename(file2)}_Len", "\n"])
+                            f"{f1}_Quant",
+                            f"{f1}_Len",
+                            f"{f2}_Quant",
+                            f"{f2}_Len", "\n"])
         o.write(header)
         writeline = "\t".join(str(stat) for stat in stats) + '\n'
         o.write(writeline)
@@ -70,6 +74,8 @@ def write_comparisons(file1, file2, output):
 
 
 if __name__ == "__main__":
+    import argparse
+    # Arguments
     parser = argparse.ArgumentParser(description="Parser")
     parser.add_argument("-1", "--In1", help="One of two files to compare to one another",
                         required=True)
@@ -79,4 +85,4 @@ if __name__ == "__main__":
                         required=True, default="")
     argument = parser.parse_args()
     write_comparisons(argument.In1, argument.In2,
-                          argument.Output)
+                      argument.Output)
